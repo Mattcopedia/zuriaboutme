541b39562efe6434853f6249a5de0cf3b63f0fcd
Mattcopedia <100870113+Mattcopedia@users.noreply.github.com>
2022-06-25 12:17:11 +0100

    Add files via upload

---
+++ zuri.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('name: Ofeimun Mathias'); 
+console.log('Height: 6.5ft');  
+console.log('Country: Nigeria');   
